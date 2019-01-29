10824604c229426473e9d9542143655493232570
Rbsann <renan.sanches@usp.br>
2019-01-29 00:48:53 -0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Several algorithms challenges, to help me learn and improve algorithms and data structures, coded in several languages, mainly in rust, clojure, scala and haskell. First i will try to solve the challenges, then after some time i will revisit them and try to improve each code with the acquired knowledge
---
+++ ep1_Ime_Usp.rs
@@ -0,0 +1,43 @@
+fn main(){
+let  a:i16 = 2;
+let  b:i16 = 100;
+let mut array_of_steps=Vec:: new();
+let c = make_array(a,b);
+for i in 0..c.len(){
+	let steps = count_steps(c[i]);
+	array_of_steps.push(steps);
+};
+println!("{:?}",array_of_steps );
+
+}
+
+fn make_array(first:i16, last:i16) ->Vec<i16>{
+	let mut array_return = Vec::new();
+	for i in first..last+1{
+		array_return.push(i);
+	}
+	return array_return;
+
+}
+
+fn count_steps(mut a:i16) -> i16{
+let mut i:i16 = 0;
+while a!=1 {
+	if a%2==0{
+		a=a/2;
+		i=i+1;
+	}else if a%2!=0{
+		a = 3*a+1;
+		i=i+1;
+	}
+
+}
+return i;
+}
+
+
+
+
+
+
+
